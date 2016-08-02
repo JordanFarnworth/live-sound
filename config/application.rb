a854c0a5a9271780fc4ec6507e3831b6d85ef576
Ben Y <jyf0000@gmail.com>
2016-08-02 07:52:00 -0600

    Setup docker build
    Refs LS-4

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module LiveSound
   class Application < Rails::Application
-    config.active_record.raise_in_transactional_callbacks = true
     config.assets.paths << Rails.root.join("public")
     config.autoload_paths += [config.root.join('lib').to_s]
 
