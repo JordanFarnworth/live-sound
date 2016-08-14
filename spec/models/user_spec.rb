b5acfea1d8de8402042df4e67823dd5fde823929
Jordan Farnworth <jordanfarn23@gmail.com>
2016-08-14 15:49:28 -0600

    few more specs
    refs LS-15

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-  # RSpec.describe User, type: :model do
-  #   pending "add some examples to (or delete) #{__FILE__}"
-  # end
+  RSpec.describe User, type: :model do
+
+    describe 'class methods' do
+      let!(:user) {FactoryGirl.create(:user, display_name: "test name")}
+
+      it 'should return the users display name' do
+        expect(user.name).to eq user.display_name
+      end
+    end
+    
+  end
